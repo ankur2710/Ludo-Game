d48c90b7b36ee0b9d158f45cdbeabe483ceb545a
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-15 23:03:32 +0530

    Update Ludo.py

--- Ludo.py
+++ Ludo.py
@@ -63,3 +63,70 @@ def OnTokenMotion(event):
     if drag_data["x"]>=444 and drag_data["x"]<=582 and drag_data["y"]>=330 and drag_data["y"]<462:
         print ('pug gayi')
 
+# put gif image on canvas
+# pic's upper left corner (NW) on the canvas is at x=50 y=10
+#canvas.create_image(50, 10, image = gif1, anchor = NW)
+
+canvas.tag_bind("oval", "<ButtonPress-1>", OnTokenButtonPress)
+canvas.tag_bind("oval", "<B1-Motion>", OnTokenMotion)
+class RollTheDice:
+    def __init__(self, parent):
+        self.dieParent = parent
+        self.dieContainer = Frame(parent).pack()
+
+        self.dieLabel = Label(self.dieContainer, text="Number of Dice you will be rolling:")
+        self.dieLabel.pack(side=TOP)
+
+        self.dieEntry = Entry(self.dieContainer)
+        self.dieEntry.pack(side=TOP)
+
+        self.sideLabel = Label(self.dieContainer, text="Number of Sides per Die:")
+        self.sideLabel.pack(side=TOP)
+
+        self.sideEntry = Entry(self.dieContainer)
+        self.sideEntry.pack(side=TOP)
+
+
+global rolldisp
+        rolldisp = StringVar()
+        self.rollResult = Label(self.dieContainer, textvariable=rolldisp)
+        self.rollResult.pack(side=TOP)
+
+        self.diceButton = Button(self.dieContainer)
+        self.diceButton.configure(text="Roll the Dice!", background="orangered1")
+        self.diceButton.pack(side=LEFT)
+        self.diceButton.bind("<Button-1>", self.diceButtonClick)
+        self.diceButton.bind("<Return>", self.diceButtonClick)
+
+        self.quitButton = Button(self.dieContainer)
+        self.quitButton.configure(text="Quit", background="blue")
+        self.quitButton.pack(side=RIGHT)
+        self.quitButton.bind("<Button-1>", self.quitButtonClick)
+        self.quitButton.bind("<Return>", self.quitButtonClick)
+
+    def diceButtonClick(self, event):
+        die = int(self.dieEntry.get())
+        side = int(self.sideEntry.get())
+        DieRoll(die, side)
+
+    def quitButtonClick(self, event):
+        self.dieParent.destroy()
+
+def DieRoll(dice, sides):
+    import random
+    rollnumber = 1
+    runningtotal = 0
+    endresult = ""
+    while rollnumber <= dice:
+        roll = random.randint(1, sides)
+        endresult += "Roll #"
+        endresult += str(rollnumber)
+        endresult += ": "
+        endresult += str(roll)
+        endresult += "\n"
+        runningtotal += roll
+        rollnumber += 1
+    finalresult = "Your Roll:\n"
+    finalresult += endresult
+    rolldisp.set(finalresult)
+
