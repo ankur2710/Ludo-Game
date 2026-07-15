5781c44eef310a12c978c6ebddf22d90581b4d33
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-15 23:02:17 +0530

    Initialize Tkinter GUI for Ludo game
    Added a basic Tkinter GUI setup for the Ludo game with a canvas and initial oval shapes.

--- Ludo.py
+++ Ludo.py
@@ -1,1 +1,29 @@
 
+from tkinter import *                                                                         #Tkinter is used as the GUI.
+import random
+root= Tk()
+
+#root.geometry('1000x1000')
+
+#base= PhotoImage(file= "ludo board.gif")
+
+#Label(root, image=base).pack(side="left")
+
+canvas = Canvas(width = 1000, height = 800, bg = 'yellow')
+root.resizable(width=False, height=False)
+
+canvas.pack(expand = YES, fill = BOTH)
+
+gif1 = PhotoImage(file = 'ludo board.gif')
+canvas.create_image(50, 10, image = gif1, anchor = NW)
+
+
+g3 = canvas.create_oval(50,290,80,320, outline="green", fill="green", tags="oval")
+        #40, 380, 90, 430
+g4 = canvas.create_oval(50,390,80,420, outline="green", fill="green", tags="oval")
+
+drag_data = {"x": 0, "y": 0, "item": None}
+init_data = {"x": 0, "y": 0, "item": None}
+final_coordinate = [0, 0]
+
+
